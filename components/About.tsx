e4527abb50f7797992c2623ccf75e0e204bd96fa
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-01 21:18:46 +0300

    Simple About section setup

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <p>
+        I&apos;m a passionate social media strategist with a deep understanding
+        of human behavior, storytelling, and brand growth. With experience
+        across multiple industries—including wellness, B2B, B2C, personal
+        branding, and design—I&apos;ve helped brands of all sizes across the
+        world build engaged communities, amplify their message, and achieve
+        measurable success. From crafting viral content that reaches millions of
+        views to developing long-term strategies that drive consistent growth, I
+        specialize in turning social media into a powerful tool for visibility
+        and impact
+      </p>
+    </div>
+  );
 }
