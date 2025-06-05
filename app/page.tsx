ad8bb4c3568f265665e06b2f2acfd3d042d59494
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-05 19:54:13 +0300

    fixing imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Header";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
 import Skills from "@/components/Skills";
