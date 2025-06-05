8f1e2bbb0285dd40cc4c0397691f33253f01cef4
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-05 19:54:38 +0300

    contact form email import

--- actions/sendEmails.ts
+++ actions/sendEmails.ts
@@ -6,7 +6,7 @@ import { Resend } from "resend";
 
 import { validateString, getErrorMeessage } from "@/lib/utils";
 
-import ContactFormEmail from "@/email/contact-form-email";
+import ContactFormEmail from "@/email/contact-form-emails";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
