b677cfdd26437271fb01646785977275eb4aed1f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-06 14:36:43 +0300

    Contact-form-email component build

--- email/contact-form-emails.tsx
+++ email/contact-form-emails.tsx
@@ -1,48 +1,65 @@
-import React from "react";
-import {
-  Html,
-  Body,
-  Head,
-  Heading,
-  Hr,
-  Container,
-  Preview,
-  Section,
-  Text,
-  ContainerProps,
-} from "@react-email/components";
+"use client";
 
-import { Tailwind } from "@react-email/tailwind";
-import { sendEmail } from "@/actions/sendEmails";
+import React, { useContext } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import clsx from "clsx";
 
-type ContactFormEmailProps = {
-  message: string;
-  senderEmail: string;
-};
+import { links } from "@/lib/data";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
-export default function ContactFormEmail({
-  message,
-  senderEmail,
-}: ContactFormEmailProps) {
+export default function Header() {
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
+
   return (
-    <Html>
-      <Head />
-      <Preview>New Message from your Portfolio</Preview>
-      <Tailwind>
-        <Body className="bg-gray-100">
-          <Container>
-            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
-              <Heading className="leading-tight">
-                You receeived thhe following meessage from the portfolio contact
-                form
-              </Heading>
-              <Text>{message}</Text>
-              <Hr />
-              <Text>The sender`&apos;`s email is {senderEmail}</Text>
-            </Section>
-          </Container>
-        </Body>
-      </Tailwind>
-    </Html>
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-80 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-slate-900 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+      <nav className=" flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap justify-center items-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex  w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition dark:text-slate-400 dark:hover:text-slate-300",
+                  {
+                    "text-slate-950 dark:text-slate-200":
+                      activeSection === link.name,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
+              >
+                {link.name}
+
+                {link.name === activeSection && (
+                  <motion.span
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                    className="bg-slate-100 dark:bg-slate-700 rounded-full absolute inset-0 -z-10"
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
