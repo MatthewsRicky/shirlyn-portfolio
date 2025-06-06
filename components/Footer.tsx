6d1180c43236590fc469394029e3235c02d03d22
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-06 14:27:42 +0300

    Footer component build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col mb-10 px-4 text-center justify-center items-center text-slate-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2023 Ricky Matthews Githinji. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website: </span>built with
+        React & Next.js(App Router & Server Actions), Typscript, Tailwind CSS,
+        Framer Motion, React Email & Resend, Vercel Hosting.
+      </p>
+    </footer>
+  );
 }
