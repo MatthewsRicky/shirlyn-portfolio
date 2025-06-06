2623175c4986bfe1e091b71ef435c9b298583a8f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-06 14:36:07 +0300

    ThemeSwitchComponent Build

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+"use client";
+
+import { useTheme } from "@/context/theme-context";
+// import React, { useEffect, useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
-  return <div>ThemeSwitch</div>;
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <button
+      className="fixed bottom-10 right-10 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex justify-center items-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-slate-950"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSun /> : <BsMoon />}
+    </button>
+  );
 }
