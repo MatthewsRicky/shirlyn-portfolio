fa96a1fab2b693645e4b9f8d4277fac8a6e141f2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-06 14:27:58 +0300

    Header component build

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  const navlinks = [
+    "Home",
+    "About",
+    "Projects",
+    "Skills",
+    "Experience",
+    "Contact",
+  ];
+  return (
+    <div className="flex my-6">
+      <ul
+        className="flex items-center justify-around
+       text-sm gap-6 font-normal bg-slate-100 w-fit shadow-lg shadow-slate-300/30 rounded-3xl px-3 py-1"
+      >
+        {navlinks.map((nav) => (
+          <li
+            key={nav}
+            className="px-3 py-3 hover:text-slate-900 active:bg-slate-100 text-slate-500"
+          >
+            <Link href={"/" + nav}>{nav}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
