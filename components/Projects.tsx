872cc9cb2059079f26d5423aa2a3dfdd6d224a54
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-06 14:32:07 +0300

    Projects component build

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,25 @@
+"use client";
+
 import React from "react";
+import SectionHeading from "./SectionHeading";
+import { projectsData } from "@/lib/data";
+import Project from "./Project";
+
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  const { ref } = useSectionInView("Projects", 0.5);
+
+  return (
+    <section ref={ref} id="projects" className="scroll-mt-28">
+      <SectionHeading>Projects</SectionHeading>
+      <div className="">
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
 }
