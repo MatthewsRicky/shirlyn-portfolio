fdadb88daca7af618a20cad0af3a3fbec03a1d2d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-06 14:35:13 +0300

    SectionHeading component build

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function SectionHeading() {
-  return <div>SectionHeading</div>;
+type SectionHeadingProps = {
+  children: React.ReactNode;
+};
+
+export default function SectionHeading({ children }: SectionHeadingProps) {
+  return (
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+      {children}
+    </h2>
+  );
 }
