eb9fa8a76a494224b6b1370cc4018340ba4d7153
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-08 15:00:54 +0300

    Sorting out the full header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,65 @@
 "use client";
 
+import React, { useContext } from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
-import React from "react";
+import clsx from "clsx";
 
-export default function Navbar() {
-  const navlinks = [
-    "Home",
-    "About",
-    "Projects",
-    "Skills",
-    "Experience",
-    "Contact",
-  ];
+import { links } from "@/lib/data";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+export default function Header() {
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
+
   return (
-    <div className="flex my-6 mx-auto items-center justify-center">
-      <ul
-        className="flex items-center justify-around
-       text-sm gap-6 font-normal bg-slate-100 w-fit shadow-lg shadow-slate-300/30 rounded-3xl px-3 py-1"
-      >
-        {navlinks.map((nav) => (
-          <li
-            key={nav}
-            className="px-3 py-3 hover:text-slate-900 active:bg-slate-100 text-slate-500"
-          >
-            <Link href={"/" + nav}>{nav}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-80 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-slate-900 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+      <nav className=" flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap justify-center items-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex  w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition dark:text-slate-400 dark:hover:text-slate-300",
+                  {
+                    "text-slate-950 dark:text-slate-200":
+                      activeSection === link.name,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
+              >
+                {link.name}
+
+                {link.name === activeSection && (
+                  <motion.span
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                    className="bg-slate-100 dark:bg-slate-700 rounded-full absolute inset-0 -z-10"
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
