8251b10c4208bf4408c886888964ece018fb9ad1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-08 07:13:24 +0300

    bg styling and centering

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Header";
+//import Hero from "@/components/Hero";
+import Intro from "@/components/Intro";
+
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
 import Skills from "@/components/Skills";
 
 export default function page() {
   return (
-    <main className="flex items-center justify-between w-full mx-autp flex-col px-4">
-      <Navbar />
-      <Hero />
+    <main className="flex items-center justify-center w-full mx-auto flex-col px-4">
+      <Intro />
       <SectionDivider />
       <About />
       <Projects />
