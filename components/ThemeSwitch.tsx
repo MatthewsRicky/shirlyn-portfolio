e801c2cba9651c7d85c92c11067c4f8f21d48e1b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-08 15:01:23 +0300

    Color edits on the theme switch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -8,7 +8,7 @@ export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
   return (
     <button
-      className="fixed bottom-10 right-10 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex justify-center items-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-slate-950"
+      className="fixed bottom-10 right-10 text-slate-950 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex justify-center items-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-slate-950 dark:text-white"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
