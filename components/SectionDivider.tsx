2b3d8aa04f89b95281dbefe7d022f8c6a7f20809
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-08 07:12:31 +0300

    centering the divider

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -10,7 +10,7 @@ export default function SectionDivider() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.15 }}
-      className="bg-slate-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
+      className="bg-slate-200 my-24 mx-auto h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
     ></motion.div>
   );
 }
