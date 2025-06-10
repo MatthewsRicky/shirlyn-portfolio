a12c696a07be654aa071a700ee34d06af2100376
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-10 23:32:53 +0300

    fixing the bg color

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className="z-999 relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-18 w-full rounded-none border border-white border-opacity-80 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-13 sm:w-xl sm:rounded-full dark:bg-slate-900 dark:border-black/40 dark:bg-opacity-75"
+        className={`fixed top-0 left-1/2 h-18 w-full rounded-none border border-white border-opacity-80 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-13 sm:w-xl sm:rounded-full dark:bg-slate-900 dark:border-black/40 dark:bg-opacity-75`}
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
