939eb6ac6bd9502e48d910a74cef54191a2c11f7
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-10 19:56:24 +0300

    uncommenting the useEffect

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -32,22 +32,22 @@ export default function ThemeContextProvider({
     }
   };
 
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+  // useEffect(() => {
+  //   const localTheme = window.localStorage.getItem("theme") as Theme | null;
 
-    if (localTheme) {
-      setTheme(localTheme);
+  //   if (localTheme) {
+  //     setTheme(localTheme);
 
-      if (localTheme === "dark") {
-        document.documentElement.classList.add("dark");
-      }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
-    } else {
-      setTheme("light");
-    }
-  }, []);
+  //     if (localTheme === "dark") {
+  //       document.documentElement.classList.add("dark");
+  //     }
+  //   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  //     setTheme("dark");
+  //     document.documentElement.classList.add("dark");
+  //   } else {
+  //     setTheme("light");
+  //   }
+  // }, []);
 
   return (
     <ThemeContext.Provider
