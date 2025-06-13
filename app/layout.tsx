6effe368e7f2e98af6d24cabc19b6a047ee967d0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-13 19:53:48 +0300

    import correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeContextProvider from "@/context/theme-context";
 import ActiveSectionContextProvider from "@/context/active-section-context";
-import Navbar from "@/components/Header";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/Footer";
 import ThemeSwitch from "@/components/ThemeSwitch";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +37,7 @@ export default function RootLayout({
         <div className="bg-[#dbd7fb] absolute -top-4 -z-10 left-[-35rem] h-125 w-200 sm:w-263 rounded-full blur-[10rem] md:left[-33rem] lg:left-[-28rem] xl:-left-60 2xl:-left-20  antialiased"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
-            <Navbar />
+            <Header />
             {children}
             <Toaster position="top-right" />
             <Footer />
