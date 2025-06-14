923945a7b37b8ca6f0384dd90913e0ed020ad938
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-14 10:09:50 +0300

    ignore es lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript: 'off'"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
